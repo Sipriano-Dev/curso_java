ceaf61ccf64b16bb55ed0e4fe2fa8fe307fa38b2
Anderson Sipriano <Siprianonew@gmail.com>
2022-10-20 15:37:42 -0300

    Introduçao ao polimorfismo

--- _001_ds_programacao_moderna/_11_heranca_polimorfismo/_01_heranca/src/application/Program.java
+++ _001_ds_programacao_moderna/_11_heranca_polimorfismo/_01_heranca/src/application/Program.java
@@ -1,24 +1,22 @@
 package application;
 
 import entities.Account;
-import entities.BusinessAccount;
 import entities.SavingsAccount;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Account acc1 = new Account(1001, "Alex", 1000.00);
-		acc1.withdraw(500.00);
-		System.out.println(acc1.getBalance());
-		
-		Account acc2 = (SavingsAccount) new SavingsAccount(1002, "Maria", 1000.00, 0.01);
-		acc2.withdraw(500.00);
-		System.out.println(acc2.getBalance());
-		
-		Account acc3 = (BusinessAccount) new BusinessAccount(1003, "Bob", 1000.00, 500.00);
-		acc3.withdraw(500.00);
-		System.out.println(acc3.getBalance());
+		Account x = new Account(1020, "Alex", 1000.0);
+		Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+		// Polimorfismo é o fato a variavel x e y ser do mesmo tipo (Account) e
+		// apontarem pra objetos diferentes, formas diferentes, isso acontece na criaçao
+		// que há em tempo de execuçao com o upcasting
+		x.withdraw(50.0);
+		y.withdraw(50.0);
+
+		System.out.println(x.getBalance());
+		System.out.println(y.getBalance());
 
 	}
 
