d37675fd5e8452b18390559f428091355f38e86f
Anderson Sipriano <Siprianonew@gmail.com>
2022-10-25 15:14:15 -0300

    Classes abstratas

---
+++ _001_ds_programacao_moderna/_11_heranca_polimorfismo/_04_classes_abstratas/src/entities/SavingsAccount.java
@@ -0,0 +1,33 @@
+package entities;
+
+public class SavingsAccount extends Account {
+
+	private Double interestRates;
+
+	public SavingsAccount() {
+		super();
+	}
+
+	public SavingsAccount(Integer number, String holder, Double balance, Double interestRates) {
+		super(number, holder, balance);
+		this.interestRates = interestRates;
+	}
+
+	public Double getInterestRates() {
+		return interestRates;
+	}
+
+	public void setInterestRates(Double interestRates) {
+		this.interestRates = interestRates;
+	}
+
+	public void updateBalance() {
+		balance += balance * interestRates;
+	}
+
+	@Override
+	public final void withdraw(double amount) {
+		balance -= amount;
+	}
+
+}
