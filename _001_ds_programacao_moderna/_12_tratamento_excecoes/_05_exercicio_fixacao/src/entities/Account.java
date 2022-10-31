10aa380485e4543276c0b9cb80aaab0f8fa29b47
Anderson Sipriano <Siprianonew@gmail.com>
2022-10-31 16:53:38 -0300

    Exercicio fixaçao - melhoria com validate

--- _001_ds_programacao_moderna/_12_tratamento_excecoes/_05_exercicio_fixacao/src/entities/Account.java
+++ _001_ds_programacao_moderna/_12_tratamento_excecoes/_05_exercicio_fixacao/src/entities/Account.java
@@ -3,14 +3,14 @@ package entities;
 import entities.exceptions.WithdrawExceptions;
 
 public class Account {
-	
+
 	private Integer number;
 	private String holder;
 	private Double balance;
 	private Double withdrawLimit;
-	
+
 	public Account() {
-		
+
 	}
 
 	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
@@ -47,20 +47,24 @@ public class Account {
 	public Double getBalance() {
 		return balance;
 	}
-	
+
 	public void deposit(Double amount) {
 		this.balance += amount;
 	}
-	
+
+	// Criando um metodo privado pra validar o valor, assim as outras classe nao tem
+	// acesso a validaçao do withdraw, fica mais seguro
 	public void withdraw(Double amount) throws WithdrawExceptions {
+		validateWithdraw(amount);
+		this.balance -= amount;
+	}
+
+	private void validateWithdraw(Double amount) throws WithdrawExceptions {
 		if (amount > withdrawLimit) {
 			throw new WithdrawExceptions("The amount exceeds withdraw limit");
 		} else if (amount > balance) {
 			throw new WithdrawExceptions("Not enough balance");
-		} else {
-			this.balance -= amount;
 		}
-		
 	}
 
 }
