78365591534204e0e08a5f70254a9d6888ef2a30
Anderson Sipriano <Siprianonew@gmail.com>
2022-11-14 00:06:04 -0300

    Try com resources (meu try, mais simples {teste})

--- _001_ds_programacao_moderna/_13_trabalhando_com_arquivos/_02_filereader_bufferedreader/src/application/Program.java
+++ _001_ds_programacao_moderna/_13_trabalhando_com_arquivos/_02_filereader_bufferedreader/src/application/Program.java
@@ -14,10 +14,8 @@ public class Program {
 		// fechar dentro do finally
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
-			String line = br.readLine();
-			while (line != null) {
-				System.out.println(line);
-				line = br.readLine();
+			while (br.ready()) {
+				System.out.println(br.readLine());
 			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
