e762eeb451993ba4f6f5f7b3bb0c80e890f43c4b
Anderson Sipriano <Siprianonew@gmail.com>
2022-11-14 00:04:47 -0300

    Try com resources (try professor)

--- _001_ds_programacao_moderna/_13_trabalhando_com_arquivos/_02_filereader_bufferedreader/src/application/Program.java
+++ _001_ds_programacao_moderna/_13_trabalhando_com_arquivos/_02_filereader_bufferedreader/src/application/Program.java
@@ -6,46 +6,21 @@ import java.io.IOException;
 
 public class Program {
 
-	//ESSE É UM JEITO MEIO ANTIGO, FECHAMENTO VERBOSO, TEM UM JEITO MAIS FÁCIL
 	public static void main(String[] args) {
 
 		String path = "D:\\1. Programming\\[Workspace]\\temp\\in.txt";
-		// stream de leitura de caracteres a partir de arquivos
-		FileReader fr = null;
-		// Mais rápido
-		BufferedReader br = null;
 
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-			
-			/* Meu jeito
-			while (br.ready()) {
-				System.out.println(br.readLine());
-			}*/
-			
-			//Jeito do professor
+		// try com recursos, já fecha pra vc nao precisar ter que usar outro try pra
+		// fechar dentro do finally
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
 			String line = br.readLine();
-			
 			while (line != null) {
 				System.out.println(line);
 				line = br.readLine();
 			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		} finally {
-			// Poder dar erro pra fechar esses dois, por isso o try
-			try {
-				if (fr != null) {
-					fr.close();
-				}
-				if (br != null) {
-					br.close();
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 		}
 
 	}
