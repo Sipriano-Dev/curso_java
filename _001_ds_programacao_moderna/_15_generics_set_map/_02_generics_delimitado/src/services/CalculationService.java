a9be4fa126f59db60650fa3424d008c13edb03f2
Anderson Sipriano <Siprianonew@gmail.com>
2022-11-28 16:50:59 -0300

    Implementaçao de genéricos limitados

--- _001_ds_programacao_moderna/_15_generics_set_map/_02_generics_delimitado/src/services/CalculationService.java
+++ _001_ds_programacao_moderna/_15_generics_set_map/_02_generics_delimitado/src/services/CalculationService.java
@@ -4,12 +4,17 @@ import java.util.List;
 
 public class CalculationService {
 
-	public static Integer max(List<Integer> list) {
+	// <T> é pra dizer que esse método vai trabalha com algum tipo T, já que nao
+	// está na classe é necessário colocar aqui
+	// Dentro do <T ...> está dizendo que extende um comparable do tipo T e que vai
+	// trabalha com um comparable T, desde que o T seja de qualquer subtipo de
+	// Comparable<T> isso é pra poder usar o compareTo
+	public static <T extends Comparable<T>> T max(List<T> list) {
 		if (list.isEmpty()) {
 			throw new IllegalStateException("List can't be empty");
 		}
-		Integer max = list.get(0);
-		for (Integer item : list) {
+		T max = list.get(0);
+		for (T item : list) {
 			if (item.compareTo(max) > 0) {
 				max = item;
 			}
