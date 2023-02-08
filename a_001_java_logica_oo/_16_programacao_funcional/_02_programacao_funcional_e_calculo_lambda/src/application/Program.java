df1e18aa7b5761b489b696932d96972f280682e8
Anderson Sipriano <siprianonew@gmail.com>
2023-02-08 16:11:56 -0300

    Programação funcional e calculo lambda

--- a_001_java_logica_oo/_16_programacao_funcional/_02_programacao_funcional_e_calculo_lambda/src/application/Program.java
+++ a_001_java_logica_oo/_16_programacao_funcional/_02_programacao_funcional_e_calculo_lambda/src/application/Program.java
@@ -6,20 +6,18 @@ import java.util.List;
 import entities.Product;
 
 public class Program {
+	public static int compareProducts(Product p1, Product p2) {
+		return p1.getPrice().compareTo(p2.getPrice());
+	}
 
 	public static void main(String[] args) {
-
 		List<Product> list = new ArrayList<>();
-
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
-
-		//Classe anônima usando comparator e expressão lambda
-		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
-
-		for (Product p : list) {
-			System.out.println(p);
-		}
+		
+		list.sort(Program::compareProducts);
+		
+		list.forEach(System.out::println);
 	}
 }
